cf42e42f44c8cc4080d3ca2cc99911a0f2d03462
aidancbrady <me@aidancbrady.com>
2015-06-30 08:50:05 -0700

    Constraint fixes, use new logo

--- Sandy Springs/MenuController.swift
+++ Sandy Springs/MenuController.swift
@@ -16,7 +16,7 @@ class MenuController: UIViewController
     {
         super.viewDidLoad()
         
-        logoImage.frame = CGRectMake(view.frame.minX+16, navigationController!.navigationBar.frame.maxY+16, view.frame.maxX-32, (view.frame.maxX-32)/2.2)
+        logoImage.frame = CGRectMake(view.frame.minX+16, navigationController!.navigationBar.frame.maxY+16, view.frame.maxX-32, (view.frame.maxX-32))
     }
     
     @IBAction func menuPressed(sender: AnyObject)
