c97f3912adf2dfc68cc0c626adc482e636934342
aidancbrady <me@aidancbrady.com>
2018-02-10 15:40:36 -0500

    Cycling blurred background images for menu

--- Sandy Springs/MenuController.swift
+++ Sandy Springs/MenuController.swift
@@ -18,24 +18,58 @@ class MenuController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var parkListButton: UIButton!
     @IBOutlet weak var amenitySearchButton: UIButton!
     
+    var imageView: UIImageView!
+    var backgroundIndex: Int = 0
+    var timer: Timer!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        logoImage.frame = CGRect(x: view.frame.width/4, y: navigationController!.navigationBar.frame.maxY+48, width: view.frame.width/2, height: view.frame.width/2)
+        let viewStretch = (view.frame.height/view.frame.width)-1.75
+        let startBoost = viewStretch*80
+        logoImage.frame = CGRect(x: view.frame.width/4, y: navigationController!.navigationBar.frame.maxY+48+startBoost, width: view.frame.width/2, height: view.frame.width/2)
         favoritesLabel.frame = CGRect(x: view.frame.maxX/2 - favoritesLabel.frame.width/2, y: logoImage.frame.maxY + 32, width: favoritesLabel.frame.width, height: favoritesLabel.frame.height)
         
         let tableStartY = favoritesLabel.frame.maxY + 8
-        let bottomHeight = CGFloat(16+42+8+42+16)
-        favoritesTable.frame = CGRect(x: view.frame.minX, y: tableStartY, width: view.frame.width, height: (view.frame.height-bottomHeight)-tableStartY)
+        let bottomHeight = CGFloat(16+42+8+42+16)+(viewStretch*80)
+        favoritesTable.frame = CGRect(x: view.frame.minX+16, y: tableStartY, width: view.frame.width-32, height: (view.frame.height-bottomHeight)-tableStartY)
         
         favoritesTable.delegate = self
         favoritesTable.dataSource = self
         favoritesTable.isScrollEnabled = true
+        favoritesTable.layer.cornerRadius = 10
+        favoritesTable.backgroundColor = UIColor(displayP3Red: 1, green: 1, blue: 1, alpha: 0.65)
         
-        parkListButton.frame = CGRect(x: view.frame.minX + 16, y: favoritesTable.frame.maxY + 16, width: view.frame.width - 32, height: 42)
+        parkListButton.frame = CGRect(x: view.frame.minX + 16, y: favoritesTable.frame.maxY + 8, width: view.frame.width - 32, height: 42)
+        parkListButton.layer.cornerRadius = 10
         
         amenitySearchButton.frame = CGRect(x: view.frame.minX + 16, y: parkListButton.frame.maxY + 8, width: view.frame.width - 32, height: 42)
+        amenitySearchButton.layer.cornerRadius = 10
+        
+        //background image
+        imageView = UIImageView(image: UIImage(named: "background_0.png"))
+        imageView.frame = view.frame
+        self.view.addSubview(imageView)
+        
+        //background blur
+        let blurEffect = UIBlurEffect(style: .regular)
+        let blurredEffectView = UIVisualEffectView(effect: blurEffect)
+        blurredEffectView.alpha = 0.9
+        blurredEffectView.frame = imageView.bounds
+        view.addSubview(blurredEffectView)
+        view.sendSubview(toBack: blurredEffectView)
+        view.sendSubview(toBack: imageView)
+        
+        //update background images
+        timer = Timer.scheduledTimer(timeInterval: 7, target: self, selector: #selector(updateBackground), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateBackground()
+    {
+        backgroundIndex = (backgroundIndex+1)%3
+        let newImage = UIImage(named: "background_" + String(backgroundIndex) + ".png")
+        UIView.transition(with: imageView, duration: 2, options: .transitionCrossDissolve, animations: {self.imageView.image = newImage}, completion: nil)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
