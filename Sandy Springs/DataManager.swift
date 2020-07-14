125160e959104efcab56f2accf1d354dac514f1a
Aidan Brady <me@aidancbrady.com>
2020-07-14 18:52:01 -0400

    Better error handling to prevent crashing on corrupt data downloads

--- Sandy Springs/DataManager.swift
+++ Sandy Springs/DataManager.swift
@@ -20,6 +20,9 @@ class DataManager {
     private let cacheDir: String
     private let cacheFile: String
     
+    private var totalDownloadCount = 0
+    private var downloaded = 0
+    
     init(delegate: DataManagerDelegate) {
         self.delegate = delegate
         connected = Utilities.isConnected()
@@ -55,14 +58,13 @@ class DataManager {
     }
     
     private func localLoadData() -> Bool {
-        if let data = URL(fileURLWithPath: cacheFile).parseJSON() {
-            loadProperties(data: data)
-            loadBackgrounds(data: data)
-            loadParks(data: data)
-            asyncLoadExtraImages()
-            return true
+        do {
+            return try loadData(url: URL(fileURLWithPath: cacheFile))
+        } catch {
+            print("Failed to load local data cache")
+            try? resetCache()
+            return false
         }
-        return false
     }
     
     private func remoteLoadData(fileData: Data) -> Bool {
@@ -71,45 +73,52 @@ class DataManager {
             try resetCache()
             try fileData.write(to: cacheURL)
             print("Downloaded file data")
+            return try loadData(url: cacheURL)
         } catch {
             print("Failed to download file data")
+            try? resetCache()
             return false
         }
-        
-        if let data = cacheURL.parseJSON() {
+    }
+    
+    private func loadData(url: URL) throws -> Bool {
+        if let data = url.parseJSON() {
+            // set the total items to download for accurate progress indication
+            totalDownloadCount += (data["backgrounds"] as! NSArray).count
+            totalDownloadCount += (data["parks"] as! NSArray).count
+            
             loadProperties(data: data)
-            loadBackgrounds(data: data)
-            loadParks(data: data)
+            try loadBackgrounds(data: data)
+            try loadParks(data: data)
             asyncLoadExtraImages()
             return true
         }
         return false
     }
     
-    private func loadParks(data: NSDictionary) {
+    private func loadParks(data: NSDictionary) throws {
         if let parks = data["parks"] as? NSArray {
-            var count = 0
             for obj in parks {
-                delegate.progressCallback(progress: Double(count) / Double(parks.count))
                 if let park = obj as? NSDictionary {
                     let parkObj = ParkData.initPark(park)
-                    loadImages(park: parkObj, cacheDir: cacheDir, primary: true)
+                    try loadImages(park: parkObj, cacheDir: cacheDir, primary: true)
+                    updateProgress()
                 }
-                count = count + 1
             }
-            delegate.progressCallback(progress: 1)
         }
     }
     
-    private func loadBackgrounds(data: NSDictionary) {
+    private func loadBackgrounds(data: NSDictionary) throws {
         if let backgrounds = data["backgrounds"] as? NSArray {
             for obj in backgrounds {
                 if let image = obj as? String {
                     let url = URL(fileURLWithPath: cacheDir + image)
                     downloadIfNeeded(image: image)
-                    
+                    updateProgress()
                     if let data = try? Data(contentsOf: url), let loadedImage = UIImage(data: data) {
                         MenuController.backgrounds.append(loadedImage)
+                    } else {
+                        throw DataError.primaryPhoto(image)
                     }
                 }
             }
@@ -149,13 +158,13 @@ class DataManager {
     private func asyncLoadExtraImages() {
         DispatchQueue.global(qos: .background).async {
             for park in Constants.parkData {
-                self.loadImages(park: park.value, cacheDir: self.cacheDir, primary: false)
+                try? self.loadImages(park: park.value, cacheDir: self.cacheDir, primary: false)
             }
             print("Completed asynchronous image load")
         }
     }
     
-    private func loadImages(park: ParkData, cacheDir: String, primary: Bool) {
+    private func loadImages(park: ParkData, cacheDir: String, primary: Bool) throws {
         var didInitial = false
         
         for image in park.imageUrls {
@@ -165,6 +174,9 @@ class DataManager {
                 downloadIfNeeded(image: image)
                 if let data = try? Data(contentsOf: url), let loadedImage = UIImage(data: data) {
                     park.images.append(loadedImage)
+                } else if primary {
+                    // fail if we can't load a primary image
+                    throw DataError.primaryPhoto(image)
                 }
             }
             if primary {
@@ -203,8 +215,17 @@ class DataManager {
         }
         try FileManager.default.createDirectory(at: URL(fileURLWithPath: cacheDir), withIntermediateDirectories: true, attributes: nil)
     }
+    
+    private func updateProgress() {
+        downloaded = downloaded + 1
+        delegate.progressCallback(progress: Double(downloaded) / Double(totalDownloadCount))
+    }
 }
 
 public protocol DataManagerDelegate {
     func progressCallback(progress: Double)
+}
+
+enum DataError: Error {
+    case primaryPhoto(String)
 }
